f94a06b10f3f2460ae180c325299c114e798ce90
Lucas Gelinas <lucas.gelinas@ocsbstudent.ca>
2021-02-24 14:48:10 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Connecting button to the function
+document.getElementById('button').addEventListener('click', check)
+let number1 = 0
+let number2 = 0
+let counter = 0
+let halifax = 0
+
+function check () {
+  number1 = document.getElementById('input1').value
+  number1 = parseInt(number1)
+  number2 = document.getElementById('input2').value
+  number2 = parseInt(number2)
+
+  for (counter; counter < number2; counter ++) {
+  halifax = halifax + number1
+  }
+  document.getElementById('answer').innerHTML = halifax
+}
